7bc921132ccbade87ea6a97dfbf281a6f48b7276
V4MF1R3 <pantvaibhav16@gmail.com>
2024-01-21 21:35:31 +0530

    deployment ready

--- app.py
+++ app.py
@@ -76,5 +76,5 @@ def submit():
     else:
         return "failure"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=8080)
